1589d4b1c9606caf598a749c81f42e1ee5c5142f
cc <cemilkabakci@gmail.com>
2020-05-16 07:02:37 +0200

    Project Repositories was added

---
+++ PPMToollBackEnd/src/main/java/io/agileintelligence/ppmtool/repository/ProjectRepository.java
@@ -0,0 +1,8 @@
+package io.agileintelligence.ppmtool.repository;
+
+import io.agileintelligence.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProjectRepository extends CrudRepository<Project,Long> {
+
+}
