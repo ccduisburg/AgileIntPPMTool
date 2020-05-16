162d98d91e43cb642a8afa044b10081f9f403366
cc <cemilkabakci@gmail.com>
2020-05-16 07:59:06 +0200

    validation

--- PPMToollBackEnd/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
+++ PPMToollBackEnd/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -1,6 +1,10 @@
 package io.agileintelligence.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -8,13 +12,21 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message="Project name is required")
     private String projectName;
+    @NotBlank(message="Project Identifier is required")
+    @Size(min=4, max=5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+    @NotBlank(message = "Project description is required")
     private String description;
+    @JsonFormat(locale = "dd-mm-yyyy")
     private Date start_date;
+    @JsonFormat(locale = "dd-mm-yyyy")
     private Date end_date;
-
+    @JsonFormat(locale = "dd-mm-yyyy")
     private Date created_At;
+    @JsonFormat(locale = "dd-mm-yyyy")
     private Date updated_At;
 
     public Project() {
